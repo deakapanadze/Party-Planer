7aa86fd42fecde8ab4eef262190b8cd6f589bdd4
dea kapanade <medea.kapanadze.1@iliauni.edu.ge>
2021-12-13 16:39:26 +0400

    Add project files.

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.fancyBox = new System.Windows.Forms.CheckBox();
+            this.healthyBox = new System.Windows.Forms.CheckBox();
+            this.costLabel = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_Cost = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 38);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // fancyBox
+            // 
+            this.fancyBox.AutoSize = true;
+            this.fancyBox.Location = new System.Drawing.Point(14, 83);
+            this.fancyBox.Name = "fancyBox";
+            this.fancyBox.Size = new System.Drawing.Size(118, 19);
+            this.fancyBox.TabIndex = 1;
+            this.fancyBox.Text = "Fancy Decoration";
+            this.fancyBox.UseVisualStyleBackColor = true;
+            // 
+            // healthyBox
+            // 
+            this.healthyBox.AutoSize = true;
+            this.healthyBox.Location = new System.Drawing.Point(12, 120);
+            this.healthyBox.Name = "healthyBox";
+            this.healthyBox.Size = new System.Drawing.Size(128, 19);
+            this.healthyBox.TabIndex = 2;
+            this.healthyBox.Text = "Healthy Decotation";
+            this.healthyBox.UseVisualStyleBackColor = true;
+            // 
+            // costLabel
+            // 
+            this.costLabel.Location = new System.Drawing.Point(12, 199);
+            this.costLabel.Name = "costLabel";
+            this.costLabel.Size = new System.Drawing.Size(100, 23);
+            this.costLabel.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Number Of People";
+            // 
+            // lbl_Cost
+            // 
+            this.lbl_Cost.AutoSize = true;
+            this.lbl_Cost.Location = new System.Drawing.Point(12, 169);
+            this.lbl_Cost.Name = "lbl_Cost";
+            this.lbl_Cost.Size = new System.Drawing.Size(31, 15);
+            this.lbl_Cost.TabIndex = 4;
+            this.lbl_Cost.Text = "Cost";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(228, 241);
+            this.Controls.Add(this.lbl_Cost);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.costLabel);
+            this.Controls.Add(this.healthyBox);
+            this.Controls.Add(this.fancyBox);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.CheckBox fancyBox;
+        private System.Windows.Forms.CheckBox healthyBox;
+        private System.Windows.Forms.TextBox costLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_Cost;
+    }
+}
+
